75e78f268d829127ec02569dce4200723d27bc4d
Louis Heath <lh16421@bristol.ac.uk>
2017-11-30 14:40:23 +0000

    collected timings for 4 workers
    TODO: get output for 1204, finalise report, submit.

--- game_of_life_parallel_rows/.build/xassert.d
+++
@@ -1,4 +0,0 @@
-xassert.o: \
-  C:/Users/jackj/Documents/GitHub/COMS20001-game-of-life/lib_xassert/src/xassert.xc \
-  C:/Users/jackj/Documents/GitHub/COMS20001-game-of-life/lib_xassert/api/xassert.h
-
